e6dde031f4a4a47abc1962b0988c1f0c9ecd067d
nsriv531 <nsriv531@mtroyal.ca>
2025-07-09 20:34:15 -0600

    utilizing uploadThing , made route and core.ts

---
+++ src/app/api/auth/[...all]/uploadthing/core.ts
@@ -0,0 +1,15 @@
+import type { FileRouter } from "uploadthing/server";
+
+// src/app/api/uploadthing/core.ts
+import { createUploadthing } from "uploadthing/server";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  profileImageUploader: f({ image: { maxFileSize: "2MB" } })
+    .onUploadComplete(async ({ file }) => {
+      console.log("Uploaded image:", file.url);
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
---
+++ src/app/api/auth/[...all]/uploadthing/route.ts
@@ -0,0 +1,10 @@
+// src/app/api/uploadthing/route.ts
+import { createRouteHandler } from "uploadthing/server";
+
+import { ourFileRouter } from "./core";
+
+const handler = createRouteHandler({
+  router: ourFileRouter,
+});
+
+export { handler as GET, handler as POST };
